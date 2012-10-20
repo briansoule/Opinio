00714d921c9d1f876ef7b03d70662cccdf26b3e4
Brian Soule <brian@soulemobile.com>
2012-10-19 21:54:05 -0500

    fresh framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opinio::Application.config.secret_token = 'd2d5f7d3f6a4b0b4711407d7f47405e4a6df54423c9ac1a484c492b4f484d67b0881d5e925b4a364e393152f0609306f8956bc21e30ab72065699ce7a782b800'
